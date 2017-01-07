c07983c3cc307321d9ab327d9a7c2421248668ff
Tsotetsi Thapelo <info@tsotetsithapelo.co.za>
2017-01-07 11:18:32 +0200

    Add remaining model serializers.

--- src/myres/apps/api/serializers.py
+++ src/myres/apps/api/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework_jwt.serializers import JSONWebTokenSerializer
 from rest_framework import serializers
 
-from myres.models import User
+from myres.validators import E164Validator
+from myres.models import User, Flat, Application, Residence, ResidenceUser, Student
 
 
 class LoginSerializer(JSONWebTokenSerializer):
@@ -13,8 +14,53 @@ class LoginSerializer(JSONWebTokenSerializer):
         super(JSONWebTokenSerializer, self).__init__(*args, **kwargs)
 
 
+class RegistrationSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=100)
+    surname = serializers.CharField(max_length=100)
+    mobile_number = serializers.CharField(max_length=16, validators=[E164Validator])
+    email = serializers.EmailField(required=True)
+    password1 = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
+
+
 class UserProfileSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
         fields = ('id', 'name', 'surname', 'gender', 'mobile_number')
+
+
+class ResidenceSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Residence
+        fields = ('name', 'email', 'type', 'capacity', 'address', 'phone_number')
+
+
+class ResidenceUserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ResidenceUser
+        fields = ('user', 'residence')
+
+
+class StudentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Student
+        fields = ('user', 'number')
+
+
+class FlatSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Flat
+        fields = ('residence', 'number', 'type', 'info')
+
+
+class ApplicationSerializer(serializers.Serializer):
+
+    class Meta:
+        model = Application
+        fields = ('flat', 'applicant', 'status', 'residence')
+
