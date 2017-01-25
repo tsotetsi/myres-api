26d9b6811d5de381017759045ffb3515f8018f3e
Thapelo <me@tsotetsithapelo.co.za>
2017-01-25 15:43:09 +0200

    Refactor.

--- src/myres/apps/api/urls.py
+++ src/myres/apps/api/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import include, url
 from rest_framework import routers
 
-from myres.apps.api.views import UserProfileView
+from myres.apps.api.views import UserView, ResidenceView, StudentView, ApplicationView, FlatView
 
 
 router = routers.DefaultRouter()
-router.register(r'user-profiles', UserProfileView, base_name='user-profiles')
+router.register(r'users', UserView, base_name='users')
+router.register(r'residences', ResidenceView, base_name='residences')
+router.register(r'students', StudentView, base_name='students')
+router.register(r'applications', ApplicationView, base_name='applications')
+router.register(r'flats', FlatView, base_name='flats')
 
 urlpatterns = [
     url(r'^', include(router.urls)),
--- src/myres/apps/api/views.py
+++ src/myres/apps/api/views.py
@@ -1,9 +1,10 @@
 from rest_framework_jwt.views import JSONWebTokenAPIView
 from rest_framework import permissions, mixins, viewsets
 
-from myres.models import User
+from myres.models import User, Residence, Student
 
-from .serializers import LoginSerializer, UserProfileSerializer
+from .serializers import LoginSerializer, UserProfileSerializer, ResidenceSerializer, StudentSerializer,\
+                         ApplicationSerializer, FlatSerializer
 
 
 class LoginView(JSONWebTokenAPIView):
@@ -14,12 +15,50 @@ class LoginView(JSONWebTokenAPIView):
     serializer_class = LoginSerializer
 
 
-class UserProfileView(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+class UserView(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     """
     View to display User profile.
     """
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.IsAuthenticated, )
     serializer_class = UserProfileSerializer
 
     def get_queryset(self):
         return User.objects.all()
+
+
+class ResidenceView(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+    """
+    View to display Residence.
+    """
+    permission_classes = (permissions.IsAuthenticated, )
+    serializer_class = ResidenceSerializer
+
+    def get_queryset(self):
+        return Residence.objects.all()
+
+
+class StudentView(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+    """
+    View to display Student.
+    """
+    permission_classes = (permissions.IsAuthenticated, )
+    serializer_class = StudentSerializer
+
+    def get_queryset(self):
+        return Student.objects.all()
+
+
+class ApplicationView(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+    """
+    View to display Applications.
+    """
+    permission_classes = (permissions.IsAuthenticated, )
+    serializer_class = ApplicationSerializer
+
+
+class FlatView(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+    """
+    View to display Flats.
+    """
+    permission_classes = (permissions.IsAuthenticated, )
+    serializer_class = FlatSerializer
