fde9204b19a846a15ebdc812454b7da22b94c950
Thapelo <me@tsotetsithapelo.co.za>
2017-02-16 08:54:24 +0200

    Add models to admin backend.

--- src/myres/admin.py
+++ src/myres/admin.py
@@ -1,8 +1,52 @@
 from django.contrib import admin
 
-from myres.models import User
+from myres.models import User, FlatType, ResidenceType, Residence, ResidenceUser, Flat, ResidenceFlat, Student, \
+                         Application
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(FlatType)
+class FlatTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ResidenceType)
+class ResidenceTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Residence)
+class ResidenceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ResidenceUser)
+class ResidenceUserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ResidenceFlat)
+class ResidenceFlatAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Flat)
+class FlatAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.site_header = "myres.co.za backend-admin"
--- src/project/settings_production.py
+++ src/project/settings_production.py
@@ -3,4 +3,4 @@ from project.settings import *
 DEBUG = False
 ALLOWED_HOSTS = ['myres.co.za', 'www.myres.co.za',]
 
-SECRET_KEY = ['z2ecjez3r@jgj-bdcuyg#38@mayl@7i+&)qx!55zgc@1e2@wo#']
+SECRET_KEY = 'z2ecjez3r@jgj-bdcuyg#38@mayl@7i+&)qx!55zgc@1e2@wo#'
