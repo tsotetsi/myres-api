5cc78a8ae73c69874f4842b4ffed381b815f4b89
Thapelo <me@tsotetsithapelo.co.za>
2017-03-31 09:46:32 +0200

    Refactor factories.

--- tests/factories.py
+++ tests/factories.py
@@ -1,57 +1,62 @@
-from factory import DjangoModelFactory, SubFactory
+import factory
 
 from myres import models
 
 
-class UserFactory(DjangoModelFactory):
+class UserFactory(factory.DjangoModelFactory):
+
+    name = factory.Sequence(lambda n: "Name %03d" % n)
+    surname = factory.Sequence(lambda n: "Surname %03d" % n)
+    mobile_number = factory.Sequence(lambda n: "+2783123456%d" % n)
 
     class Meta:
         model = models.User
+        django_get_or_create = ('name', 'surname', 'gender', 'mobile_number')
 
 
-class FlatTypeFactory(DjangoModelFactory):
+class FlatTypeFactory(factory.DjangoModelFactory):
 
     class Meta:
         model = models.FlatType
 
 
-class ResidenceTypeFactory(DjangoModelFactory):
+class ResidenceTypeFactory(factory.DjangoModelFactory):
 
     class Meta:
         model = models.ResidenceType
 
 
-class ResidenceFactory(DjangoModelFactory):
+class ResidenceFactory(factory.DjangoModelFactory):
 
     class Meta:
         model = models.Residence
 
 
-class ResidenceUserFactory(DjangoModelFactory):
+class ResidenceUserFactory(factory.DjangoModelFactory):
 
     class Meta:
         model = models.ResidenceUser
 
 
-class FlatFactory(DjangoModelFactory):
+class FlatFactory(factory.DjangoModelFactory):
 
     class Meta:
         model = models.Flat
 
 
-class ResidenceFlatFactory(DjangoModelFactory):
+class ResidenceFlatFactory(factory.DjangoModelFactory):
 
     class Meta:
         model = models.ResidenceFlat
 
 
-class StudentFactory(DjangoModelFactory):
+class StudentFactory(factory.DjangoModelFactory):
 
     class Meta:
         model = models.Student
 
 
-class ApplicationFactory(DjangoModelFactory):
+class ApplicationFactory(factory.DjangoModelFactory):
 
     class Meta:
         model = models.Application
