887b2004804f3da8bf8a8af9399bbdc8a3c97519
Thapelo <me@tsotetsithapelo.co.za>
2017-04-03 11:14:51 +0200

    Refactor tests.

--- tests/unit/test_api_urls.py
+++ tests/unit/test_api_urls.py
@@ -2,17 +2,15 @@ from django.test import TestCase
 from rest_framework import status
 
 
-URLS_API = [
-    '/api/students/',
-    '/api/applications/',
-    '/api/flats/',
-    '/api/residences/',
-    '/api/users/',
-]
-
-
 class APISecureURLTestCase(TestCase):
     def test_secure_api_urls(self):
-        for url in URLS_API:
+        secure_urls = [
+            '/api/students/',
+            '/api/applications/',
+            '/api/flats/',
+            '/api/residences/',
+            '/api/users/',
+        ]
+        for url in secure_urls:
             response = self.client.get(url)
             self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
