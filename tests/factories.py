315e34c9cadf426690a3f3bcc7a08d9445617821
Thapelo <me@tsotetsithapelo.co.za>
2017-04-03 13:53:24 +0200

    Add attributes to factories.

--- tests/factories.py
+++ tests/factories.py
@@ -1,6 +1,11 @@
 import factory
+import factory.fuzzy
+from faker import Factory as FakerFactory
+from random import random
 
 from myres import models
+
+faker = FakerFactory.create()
 
 
 class UserFactory(factory.DjangoModelFactory):
@@ -8,6 +13,8 @@ class UserFactory(factory.DjangoModelFactory):
     name = factory.Sequence(lambda n: "Name %d" % n)
     surname = factory.Sequence(lambda n: "Surname %d" % n)
     mobile_number = factory.Sequence(lambda n: "+2783000000%d" % n)
+    email = factory.LazyAttribute(lambda o: '%s@university.ac.za' % o.name)
+    gender = random.choices['MALE', 'FEMALE']
 
     class Meta:
         model = models.User
@@ -15,48 +22,72 @@ class UserFactory(factory.DjangoModelFactory):
 
 
 class FlatTypeFactory(factory.DjangoModelFactory):
+    name = factory.Sequence(lambda n: "Flat type %d" % n)
+    description = factory.LazyAttribute(lambda x: faker.sentence(nb_words=6))
 
     class Meta:
         model = models.FlatType
 
 
 class ResidenceTypeFactory(factory.DjangoModelFactory):
+    name = factory.Sequence(lambda n: "Residence type %d" % n)
+    description = factory.LazyAttribute(lambda x: faker.sentence(nb_words=6))
 
     class Meta:
         model = models.ResidenceType
 
 
 class ResidenceFactory(factory.DjangoModelFactory):
+    name = factory.Sequence(lambda n: "Residence %d" % n)
+    email = factory.LazyAttribute(lambda o: '%s@university.ac.za' % o.name.strip(''))
+    type = factory.SubFactory(ResidenceTypeFactory)
+    capacity = factory.fuzzy.FuzzyInteger(100, 400)
+    address = faker('address')
+    phone_number = factory.Sequence(lambda n: "+2721000000%d" % n)
 
     class Meta:
         model = models.Residence
 
 
 class ResidenceUserFactory(factory.DjangoModelFactory):
+    user = factory.SubFactory(UserFactory)
+    residence = factory.SubFactory(ResidenceFactory)
 
     class Meta:
         model = models.ResidenceUser
 
 
 class FlatFactory(factory.DjangoModelFactory):
+    residence = factory.SubFactory(ResidenceFlatFactory)
+    number = factory.sequence(lambda s: "10%dA" % s)
+    type = factory.SubFactory(FlatTypeFactory)
+    info = factory.LazyAttribute(lambda s: faker.sentence(nb_words=6))
 
     class Meta:
         model = models.Flat
 
 
 class ResidenceFlatFactory(factory.DjangoModelFactory):
+    residence = factory.SubFactory(ResidenceFactory)
+    flat = factory.SubFactory(FlatFactory)
 
     class Meta:
         model = models.ResidenceFlat
 
 
 class StudentFactory(factory.DjangoModelFactory):
+    user = factory.SubFactory(UserFactory)
+    number = factory.LazyAttribute(lambda o: 'abcde01%s' % o.user.name.strip(''))
 
     class Meta:
         model = models.Student
 
 
 class ApplicationFactory(factory.DjangoModelFactory):
+    flat = factory.SubFactory(FlatFactory)
+    applicant = factory.SubFactory(UserFactory)
+    status = random.choices["NEW", "REVIEW"]
+    residence = factory.SubFactory(ResidenceFactory)
 
     class Meta:
         model = models.Application
