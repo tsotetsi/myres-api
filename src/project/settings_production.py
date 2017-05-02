6236cbe1351135dd17942594e0c0a155de78fc27
Thapelo <me@tsotetsithapelo.co.za>
2017-05-02 13:36:36 +0200

    Add IP address to production settings.

--- src/project/settings_production.py
+++ src/project/settings_production.py
@@ -1,6 +1,6 @@
 from project.settings import *
 
 DEBUG = False
-ALLOWED_HOSTS = ['myres.co.za', 'www.myres.co.za',]
+ALLOWED_HOSTS = ['154.127.59.170', 'myres.co.za', 'www.myres.co.za', ]
 
-SECRET_KEY = 'z2ecjez3r@jgj-bdcuyg#38@mayl@7i+&)qx!55zgc@1e2@wo#'
+SECRET_KEY = 'z2ecjez3r@jgj-bdcuyg#38@mayl@7i+&)qx!55zgc@1e2@wo#ad94@m(n'
